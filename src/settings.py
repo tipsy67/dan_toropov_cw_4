703f11304150ce5983744691c721d42e552ac487
dan.toropov <dan.toropov@gmail.com>
2024-06-08 23:31:06 +0300

    pep reformat

--- src/settings.py
+++ src/settings.py
@@ -7,8 +7,7 @@ URL_CURRENCY = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1
 
 # https://api.hh.ru/openapi/redoc#tag/Poisk-vakansij/operation/get-vacancies
 VACANCY_SEARCH_FIELDS = {'1': {'name'}, '2': {'name', 'description'}}
-VACANCY_SEARCH_PER_PAGE = 100 #100
-VACANCY_SEARCH_PAGE = 20 # 20
+VACANCY_SEARCH_PER_PAGE = 100  # 100
+VACANCY_SEARCH_PAGE = 20  # 20
 
 TAGS_FOR_REMOVE = {'<highlighttext>', '</highlighttext>'}
-
